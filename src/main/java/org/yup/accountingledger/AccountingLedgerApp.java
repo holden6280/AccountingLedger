7a3fa51f57a70ae8ba876b7bcc20a277bdccb390
holden6280 <emiicleghorn@gmail.com>
2023-04-28 15:42:09 -0400

    added another line of output

--- src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
+++ src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
@@ -5,6 +5,7 @@ public class AccountingLedgerApp {
     public static void main(String[] args){
 
         System.out.println("anything");
+        System.out.println("This is the new text I want to show.");
 
     }
 
