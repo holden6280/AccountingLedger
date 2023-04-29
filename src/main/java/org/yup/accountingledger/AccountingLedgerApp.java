8ecf115ef869cead54205eb6eb629f5fe83b0c2f
holden6280 <emiicleghorn@gmail.com>
2023-04-29 17:34:18 -0400

    scaffolding

--- src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
+++ src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
@@ -4,9 +4,19 @@ public class AccountingLedgerApp {
 
     public static void main(String[] args){
 
-        System.out.println("anything");
-        System.out.println("This is the new text I want to show.");
+        //ledger method goes here
+        //Ledger - All entries should show the newest entries first
+        // A) All - Display all entries
+        // D) Deposits - Display only the entries that are deposits into the account
+        // P) Payments - Display only the negative entries (or payments)
+        // R) Reports - A new screen that allows the user to run pre-defined reports or to run a custom search
+        //  1) Month To Date
+        //  2) Previous Month
+        //  3) Year To Date
+        //  4) Previous Year
+        //  5) Search by Vendor - prompt the user for the vendor name and display all entries for that vendor
+        //  0) Back - go back to the report page
+        // H) Home - go back to the home page
 
     }
-
 }
---
+++ src/main/java/org/yup/accountingledger/HomeScreen.java
@@ -0,0 +1,13 @@
+package org.yup.accountingledger;
+
+public class HomeScreen {
+
+    //The home screen should give the user the following options. The application should continue to run until the user chooses to exit.
+    // D) Add Deposit - prompt user for the deposit information and save it to the csv file
+    // P) Make Payment (Debit) - prompt user for the debit information and save it to the csv file
+    // L) Ledger - display the ledger screen
+    // X) Exit - exit the application
+
+    //this should contain a system.in switch statement
+
+}
