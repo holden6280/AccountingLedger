87830de8d41625066f6b6df230679922b34bd6b8
holden6280 <emiicleghorn@gmail.com>
2023-04-29 18:59:58 -0400

    first commit

--- src/main/java/org/yup/accountingledger/Debit.java
+++ src/main/java/org/yup/accountingledger/Debit.java
@@ -20,4 +20,44 @@ public class Debit {
         this.amount = amount;
 
     }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
